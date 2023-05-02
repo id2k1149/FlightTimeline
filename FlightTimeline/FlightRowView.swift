cde88e667c8c41c0d2cc685e159238c8752266a5
id2k1149 <id2k1149@gmail.com>
2023-05-02 16:08:52 -0400

    refactor FlightRowView

--- FlightTimeline/FlightRowView.swift
+++ FlightTimeline/FlightRowView.swift
@@ -8,25 +8,17 @@
 import SwiftUI
 
 struct FlightRowView: View {
-    @State private var isPresented = false
     let flight: FlightInformation
     
     var body: some View {
-        Button(action: { isPresented.toggle() }) {
-            HStack {
-                Text("\(flight.airline) \(flight.number)")
-                    .frame(width: 120, alignment: .leading)
-                Text(flight.otherAirport)
-                    .frame(alignment: .leading)
-                Spacer()
-                Text(flight.flightStatus)
-                    .frame(alignment: .trailing)
-                
-            }
-            .sheet(isPresented: $isPresented) {
-                FlightDetailsView(isPresented: $isPresented,
-                                  flight: flight)
-            }
+        HStack {
+            Text("\(flight.airline) \(flight.number)")
+                .frame(width: 120, alignment: .leading)
+            Text(flight.otherAirport)
+                .frame(alignment: .leading)
+            Spacer()
+            Text(flight.flightStatus)
+                .frame(alignment: .trailing)
         }
     }
 }
