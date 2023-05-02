a969e8ac81c4f52d038c5db2c456bf4ed4a5b1a0
id2k1149 <id2k1149@gmail.com>
2023-05-02 12:47:25 -0400

    add NavigationStack

--- FlightTimeline/HomeScreenView.swift
+++ FlightTimeline/HomeScreenView.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct HomeScreenView: View {
+    private let flightInfo = FlightInformation.generateFlights()
+    
     var body: some View {
-        Image(systemName: "airplane")
-            .resizable()
-            .frame(width: 250, height: 250)
-            .opacity(0.1)
-            .rotationEffect(.degrees(-90))
+        NavigationStack {
+            ZStack {
+                Image(systemName: "airplane")
+                    .resizable()
+                    .frame(width: 250, height: 250)
+                    .opacity(0.1)
+                    .rotationEffect(.degrees(-90))
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    NavigationLink("Arrivals") {
+                        FlightBoardView(boardName: "Arrivals")
+                    }
+                    
+                    NavigationLink("Departures") {
+                        FlightBoardView(boardName: "Departures")
+                    }
+                    
+                    NavigationLink("Flight Timeline") {
+                        TimelineView(flights: flightInfo)
+                    }
+                    Spacer()
+                }
+                .font(.title)
+                .padding()
+            }
+        }
     }
 }
 
